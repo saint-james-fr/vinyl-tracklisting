3b2edb8995b4c88ebd64403804f8581132e3ce7b
saint-james-fr <maxencerobinet@gmail.com>
2022-09-06 03:28:10 +0200

    belle refactorisation avant d'aller dormir

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-Sortable.create(faceA, {
+Sortable.create(sideA, {
 	animation: 200,
 	handle: ".handle-function",
 	group: "shared",
@@ -6,17 +6,20 @@ Sortable.create(faceA, {
 	swapClass: "highlight",
 });
 
-updateNumberOfTracks();
+// window.onbeforeunload = function(){ return 'Do you want to reload the page?';} // cool security to add
 
 
-
+updateNumberOfTracks("sideA");
 var handlerNumber;
 var numberOfTracks;
 var positionNumber;
 var timeWrapperNumber;
+
+
+// Update Position when changes happen
 var mutationObserver = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
-    updatePosition();
+    updatePosition("sideA");
   });
 });
 mutationObserver.observe(document.documentElement, {
@@ -29,39 +32,41 @@ mutationObserver.observe(document.documentElement, {
 });
 
 
-function updateNumberOfTracks() {
-	numberOfTracks = document.getElementById("faceA").children.length;
+function updateNumberOfTracks(side) {
+	numberOfTracks = document.getElementById(`${side}`).children.length;
 }
 
-function updatePosition() {
+function updatePosition(side) {
+	var sideLetter = side.slice(4)
 	positionElements = document.getElementsByClassName("position")
 	for (let i = 0; i < positionElements.length; i++) {
 		a = document.getElementById(positionElements[i].id);
 		newNumber = i + 1		
-		a.textContent = `A${newNumber}`		
+		a.textContent = `${sideLetter}${newNumber}`		
 	}
 }
 
-function addTitle() {
-	  	updateNumberOfTracks();
-	  	handlerNumber = numberOfTracks + 1;	  	
-		createHandler(`handler ${handlerNumber}`);
+function addTitle(side) {
+		var sideLetter = side.slice(4);
+	  updateNumberOfTracks(`${side}`);
+	  handlerNumber = numberOfTracks + 1;	  	
+		createHandler(`handler ${handlerNumber}`,`${side}`);
 		positionNumber = handlerNumber;
-		createPosition(`position A${positionNumber}`);		
+		createPosition(`position ${side}${positionNumber}`,`${sideLetter}`);		
 		createIcon();
 		createTitle(`title ${positionNumber}`);
-		timeWrapperNumber = `timeWrapper ${positionNumber}`;
-		createTimeWrapper(timeWrapperNumber);
+		timeWrapperNumber = handlerNumber;
+		createTimeWrapper(`timeWrapper ${positionNumber}`);
 		createMinute(`minute ${positionNumber}`)
 		createSeparator(`separator ${positionNumber}`);
 		createSecond(`second ${positionNumber}`);	
 }
 
-function removeTitle() {
-	updateNumberOfTracks()
-	lastPosition = faceA.length
-	let lastTitle = document.getElementById("faceA").lastElementChild;
-	if (lastPosition !== null) {
+function removeTitle(side) {
+	updateNumberOfTracks(`${side}`)
+	lastPosition = numberOfTracks
+	let lastTitle = document.getElementById(`${side}`).lastElementChild;
+	if (lastPosition > 1) {
 	lastTitle.remove();
 	}
 }
@@ -75,16 +80,16 @@ function newElement(el, classes, sourceId, newId) {
 	el.setAttribute("id", newId);
 }
 
-function createHandler(handlerId) {
+function createHandler(handlerId,side) {
 	newElement(
 		"div",
 		["list-group-item", "handle-function", "row"],
-		"faceA",
+		side,
 		handlerId
 		)	
 }
 
-function createPosition(positionId) {
+function createPosition(positionId,side) {
 	newElement(
 		"span",
 		["position", "col-1"],
@@ -92,7 +97,7 @@ function createPosition(positionId) {
 		positionId
 		)
 	a = document.getElementById(positionId)
-	a.textContent = `A${positionNumber}`
+	a.textContent = `${side}${positionNumber}`
 }
 
 function createIcon() {
@@ -133,6 +138,10 @@ function createMinute(minuteId) {
 		)
 	document.getElementById(minuteId).setAttribute("placeholder", "mm");
 	document.getElementById(minuteId).setAttribute("type", "number");
+	document.getElementById(minuteId).setAttribute("min", "0");
+	document.getElementById(minuteId).setAttribute("max", "59");
+	document.getElementById(minuteId).setAttribute("oninput", "this.value = !!this.value && Math.abs(this.value) >= 0 ? Math.abs(this.value) : null");	
+
 }
 
 function createSecond(secondId) {
@@ -144,6 +153,9 @@ function createSecond(secondId) {
 		)
 	document.getElementById(secondId).setAttribute("placeholder", "ss");
 	document.getElementById(secondId).setAttribute("type", "number");
+	document.getElementById(secondId).setAttribute("min", "0");
+	document.getElementById(minuteId).setAttribute("max", "59");
+	document.getElementById(secondId).setAttribute("oninput", "this.value = !!this.value && Math.abs(this.value) >= 0 ? Math.abs(this.value) : null");	
 }
 
 function createSeparator(separatorId) {
