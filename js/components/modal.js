a90c61f9691f76a3efe603069ae8c4b03ced234e
Maxence Robinet <maxencerobinet@gmail.com>
2022-12-15 18:19:35 +0100

    content added 1/2 / css improvements and modal

--- js/components/modal.js
+++ js/components/modal.js
@@ -1,22 +1,21 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("modalTechnical");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementById("close");
 
 // When the user clicks the button, open the modal
 btn.onclick = function () {
   modal.style.display = "block";
 };
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
+  console.log("hello")
   modal.style.display = "none";
 };
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
