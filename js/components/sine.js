eaebf6a2f66780005d6490c3170d49b8d92d62a8
Maxence Robinet <maxencerobinet@gmail.com>
2022-12-16 15:21:50 +0100

    tracklisting

--- js/components/sine.js
+++ js/components/sine.js
@@ -7,21 +7,21 @@
       distance: 2,
       radius: 6,
       height: 100,
-      span: Math.PI * 2.25,
+      span: Math.PI * 4,
     },
     width = opts.amount * (opts.radius * 2 + opts.distance),
     arr = new Array(opts.amount).fill().map((el, ind) => {
       return {
         a: (opts.span / opts.amount) * ind,
         x: (opts.radius * 2 + opts.distance) * ind,
-        c: "hsl(th, 20%, 80%)",
+        c: "hsl(29, 63%, 51%)",
       };
     });
   function loop() {
     $.fillStyle = "black";
     $.fillRect(0, 0, w, h);
     arr.forEach((el) => {
-      el.a += (Math.PI / 180) * 3;
+      el.a += (Math.PI / 180) * 2.2;
       $.beginPath();
       $.arc(
         el.x - width / 2 + w / 2,
